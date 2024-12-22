35300c30c7300955f228a4f0888003c4d354868c
Lars Milz <milzlars88@gmail.com>
2024-12-22 20:59:30 +0100

    delete comments

--- src/Pomodoro/Pomodoro.cpp
+++ src/Pomodoro/Pomodoro.cpp
@@ -1,10 +1,8 @@
 #include "Pomodoro.h"
 
-// Constructor
 Pomodoro::Pomodoro(int focustime, int breaktime)
     : focustime(focustime * 60), breaktime(breaktime) {}
 
-// Function for start the pomodoro timer
 bool Pomodoro::start() {
   std::cout << "\033[1;1H";  // Cursor nach oben links setzen
   std::cout << "\rVerbleibende Zeit: " << focustime / 60 << " Minuten "
@@ -20,10 +18,8 @@ bool Pomodoro::start() {
   return true;
 }
 
-// Setter function for focus time
 void Pomodoro::setFocusTime(const int new_focustime) {
   focustime = new_focustime * 60;
 }
 
-// Getter function for focus time
 int Pomodoro::getFocusTime() const { return focustime; }
