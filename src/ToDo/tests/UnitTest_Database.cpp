af047fc19558fd4df063adab13fe46b27d1f2aa8
Lars Milz <milzlars88@gmail.com>
2024-12-25 23:16:11 +0100

    transform todo into google test setup

--- src/ToDo/tests/UnitTest_Database.cpp
+++ src/ToDo/tests/UnitTest_Database.cpp
@@ -22,19 +22,15 @@
 
 #include <filesystem>
 
-#include "CppUTest/TestHarness.h"
+#include <gtest/gtest.h>
 #include "Database.h"
-
-TEST_GROUP(DatabaseTest){void setup() override{}
-
-                         void teardown() override{}};
 
 TEST(DatabaseTest, OpenDatabase)
 {
     Database* db = new Database(":memory:");  // create data in memory
     sqlite3* connection = db->getConnection();
 
-    CHECK(connection != nullptr);
+    EXPECT_NE(connection, nullptr);
 
     delete db;
 }
--- src/ToDo/tests/UnitTest_ToDo.cpp
+++ src/ToDo/tests/UnitTest_ToDo.cpp
@@ -22,12 +22,13 @@
 
 #include <algorithm>
 
-#include "gtest/gtest.h"
+#include <gtest/gtest.h>
 #include "ToDo.h"
 #include "ToDoList.h"
 
-TEST(ToDoTests, AddToDos)
+TEST(ToDoListTest, AddToDos)
 {
+    ToDoList* test_obj = new ToDoList();
     ToDo todo1;
     ToDo todo2;
     // ToDo 1
@@ -40,4 +41,49 @@ TEST(ToDoTests, AddToDos)
     todo2.description = "Test Task 2 Description";
     todo2.status = "Done";
     todo2.due_date = "31.1.2025";
+
+    test_obj->addItem(todo1);
+    test_obj->addItem(todo2);
+    auto list = test_obj->showList();
+
+    EXPECT_EQ(list[0].name, "Test Task 1");
+    EXPECT_EQ(list[0].description, "Test Task 1 Description");
+    EXPECT_EQ(list[0].status, "Todo");
+    EXPECT_EQ(list[0].due_date, "1.1.2025");
+
+    EXPECT_EQ(list[1].name, "Test Task 2");
+    EXPECT_EQ(list[1].description, "Test Task 2 Description");
+    EXPECT_EQ(list[1].status, "Done");
+    EXPECT_EQ(list[1].due_date, "31.1.2025");
+
+    delete test_obj;
+}
+
+TEST(ToDoListTest, RemoveToDo)
+{
+    ToDoList* test_obj = new ToDoList();
+    ToDo todo1;
+    ToDo todo2;
+    // ToDo 1
+    todo1.name = "Test Task 1";
+    todo1.description = "Test Task 1 Description";
+    todo1.status = "Todo";
+    todo1.due_date = "1.1.2025";
+    // ToDo 2
+    todo2.name = "Test Task 2";
+    todo2.description = "Test Task 2 Description";
+    todo2.status = "Done";
+    todo2.due_date = "31.1.2025";
+
+    test_obj->addItem(todo1);
+    test_obj->addItem(todo2);
+    test_obj->removeItem(todo1);
+    auto list = test_obj->showList();
+
+    EXPECT_EQ(list[0].name, "Test Task 2");
+    EXPECT_EQ(list[0].description, "Test Task 2 Description");
+    EXPECT_EQ(list[0].status, "Done");
+    EXPECT_EQ(list[0].due_date, "31.1.2025");
+
+    delete test_obj;
 }
