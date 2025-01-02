907f85a29e43b4a395af99636dfa15c369127a97
Lars Milz <milzlars88@gmail.com>
2025-01-02 15:40:28 +0100

    fixing further clang tidy warnings

--- src/app/src/PomoTaskApp.cpp
+++ src/app/src/PomoTaskApp.cpp
@@ -21,8 +21,6 @@
 // SOFTWARE.
 
 // Includes
-#include "PomoTaskApp.h"
-
 #include <sys/ioctl.h>
 #include <unistd.h>
 
@@ -36,6 +34,7 @@
 #include <thread>
 #include <utility>
 
+#include "PomoTaskApp.h"
 #include "MatrixEffect.h"
 #include "Pomodoro.h"
 #include "RainEffect.h"
